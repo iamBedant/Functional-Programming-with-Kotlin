7d347fd66cb5ff7d1d71b78b75dd1ffcf91106ca
iamBedant <bebi2527@live.in>
2016-11-06 14:52:47 +0530

    feat: New Feature
    Added Function which returns a function

--- higherOrderFunction.kt
+++ higherOrderFunction.kt
@@ -2,6 +2,11 @@ fun main(args: Array<String>) {
 	
    validateValueWithFunc("bebi2527@gmail.com",::parseEmail,"Email")
    validateValueWithFunc("7204773534",::parsePhoneNo,"Phone")
+
+   validateValueWithFunc("bebi2527@gmail.com",returnMyFunction(1),"Email")
+   validateValueWithFunc("7204773534",returnMyFunction(2),"Phone")
+
+   if(1==1) println ("Print Successful")
 }
 
 fun parseEmail(address:String): Boolean{
@@ -19,4 +24,11 @@ fun validateValueWithFunc(value: String, parseFunc: (value:String) -> Boolean, t
         println("Invalid $type");
     else
         println("Valid $type");
+}
+
+fun returnMyFunction(type:Int):(String)->Boolean{
+	if(type ==1)
+	return ::parseEmail
+	else
+	return ::parsePhoneNo
 }
